3ad07793849a126a7ed059cf9cf95c7643fb74c4
Yuriy Shefer <sheff@sheffmachine.com>
2015-12-27 18:16:48 -0800

    Fix sign up issue requiring being logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
-  before_action :require_user, except: [:index, :show]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
