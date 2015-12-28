707af4e3361bacb0501b1ab2b839f0d8b6fe4bae
Yuriy Shefer <sheff@sheffmachine.com>
2015-12-27 18:13:54 -0800

    update current user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,8 @@ class ArticlesController < ApplicationController
     
   end
   def create
-    debugger
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
@@ -36,7 +35,6 @@ class ArticlesController < ApplicationController
 
   end
   def destroy
-
     @article.destroy
     flash[:danger] = "Article has been successfully deleted."
     redirect_to articles_path
